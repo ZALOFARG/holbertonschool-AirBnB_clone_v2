37fa8c8d6dfeacca164e7af8c4cba4578aa65a50
ZALOFARG <andre.work.go@gmail.com>
2024-04-14 23:58:11 -0500

    fix: inserting comments on script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,19 @@
+#!/usr/bin/python3
+
+-- check if the db already exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- create a new user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- grant USAGE privilege to the user hbnb_dev on all dbs
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 
+-- grant all privileges to the user hbnb_dev on the db hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
+-- grant SELECT privileges to the user hbnb_dev on the performance_schema db
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
+-- update privileges
 FLUSH PRIVILEGES;
