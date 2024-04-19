e3146c69da5d57ce37176bf2119c16b2acf6970f
ZALOFARG <andre.work.go@gmail.com>
2024-04-19 01:34:29 -0500

    create all the logic for a ORM success and also create another type of storage (DBStorage)

---
+++ models/engine/db_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the class DBStorage"""
+
+class DBStorage:
+    """asd"""
+    __engine = None
+    __session = None
+
+
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if storage_type == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City',
+                              backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+    if storage_type != 'db':
+        @property
+        def cities(self):
+            """Getter for list of city instances related to the state"""
+            from models import storage
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
