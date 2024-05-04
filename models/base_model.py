21d483f71f2b8807e4db297ad3881a4539175f9a
ZALOFARG <andre.work.go@gmail.com>
2024-05-03 20:34:11 -0500

    fix: indentation with filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -22,28 +22,29 @@ class BaseModel:
         updated_at = Column(DateTime, default=datetime.utcnow)
 
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(
-                        kwargs["created_at"], time_format)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(
-                        kwargs["updated_at"], time_format)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
+    else:
+        def __init__(self, *args, **kwargs):
+            """Instantiates a new model"""
+            if not kwargs:
                 self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                for key, value in kwargs.items():
+                    if key != "__class__":
+                        setattr(self, key, value)
+                if kwargs.get("created_at", None) and type(self.created_at) is str:
+                    self.created_at = datetime.strptime(
+                            kwargs["created_at"], time_format)
+                else:
+                    self.created_at = datetime.utcnow()
+                if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                    self.updated_at = datetime.strptime(
+                            kwargs["updated_at"], time_format)
+                else:
+                    self.updated_at = datetime.utcnow()
+                if kwargs.get("id", None) is None:
+                    self.id = str(uuid.uuid4())
 
 
     def __str__(self):
